5d7ffc499df78c7aebd797176b317b6543e5f182
m351351 <turtinen.merituulia@gmail.com>
2025-11-26 09:35:59 +0200

    toimiva k-means

--- phase2/Ohjelma K-means.py
+++ phase2/Ohjelma K-means.py
@@ -9,6 +9,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
+
+#####################
+# lisäsin tämän tänne ylös, vaiheesta kolme
+#####################
+def update_centroids(number_of_clusters, np_data, clusters):
+    new_centroids = np.zeros((number_of_clusters, np_data.shape[1]))
+   
+    for i in range(number_of_clusters):
+        cluster_points = np_data[clusters == i]
+       
+        if len(cluster_points) > 0:
+           new_centroids[i] = np.mean(cluster_points, axis=0)
+        else:
+            random_index = np.random.randint(0, len(np_data))
+            new_centroids[i] = np_data[random_index]
+            print(f"Klusteri {i} oli tyhjä -> arvottiin uusi centroidi:", new_centroids[i])
+            
+    return new_centroids
+#####################
+
+
 #############
 ## vaihe 1 ##
 #############
@@ -60,42 +81,55 @@ random_data = np.stack((random_x, random_y, random_z), axis=1)
 
 print("randomit: \n", random_data)
 
+#############
+## RULETTI ##
+#############
+
+all_centroids = random_data.shape[0]
+max_rounds = 100
+current_centroids = random_data # siirretään randomit currenteiksi niin saadaan ne mukavasti käyttöön
+
 
 #############
 ## vaihe 2 ##
-#############
+#############0
 
 # tee for-loop joka tsekkaa cenrtoid-pointit ja vertaa jokaista pointtia kaikkiin data_pointteihin
 # laske vertaa jokaista random_data jokaiseen data_pointsiin
 # tallenna kaikki tulokset johonkin arrayhin ja etsi minimitulos
 # julista pienin arvo voittajaksi
+for rounds in range(max_rounds):
+    clusters = []
+    max_value = np.max(np_data)
 
-clusters = []
-max_value = np.max(np_data)
+    closest_data_centroid = 0
 
-closest_data_centroid = 0
-    
-for data_point_index, D_centroid_point in enumerate(np_data):
-    
-    min_distance = max_value #tässä voisi käyttää myös infinitiä, mutta laitoin maximin joka datasta löytyy
-    closest_data_centroid = -1 #laitetaan -1 niin virheen sattuessa huomataan
-     
+    old_centroids = current_centroids #siirrellään tän avulla centroideja lähemmäksi kohdetta
         
-    for R_centroid_index, R_centroid_point in enumerate(random_data):
+    for data_point_index, D_centroid_point in enumerate(np_data):
         
-        distance = np.linalg.norm(D_centroid_point - R_centroid_point)
-        
-        if distance < min_distance: #jos tämä etäisyys pienempi kuin pienin mahd. etäisyys
-            min_distance = distance #päivitetään min_distance distanceksi ja 
-            closest_data_centroid = R_centroid_index #closest_data_centroid R_centroid_indeksiksi
+        min_distance = max_value #tässä voisi käyttää myös infinitiä, mutta laitoin maximin joka datasta löytyy
+        closest_data_centroid = -1 #laitetaan -1 niin virheen sattuessa huomataan
         
             
-    clusters.append(closest_data_centroid)
+        for R_centroid_index, R_centroid_point in enumerate(old_centroids): #vaihdoin sulkeisiin random_datan tilalle old_centroids jotta kierrättäminen toimii
+            
+            distance = np.linalg.norm(D_centroid_point - R_centroid_point)
+            
+            if distance < min_distance: #jos tämä etäisyys pienempi kuin pienin mahd. etäisyys
+                min_distance = distance #päivitetään min_distance distanceksi ja 
+                closest_data_centroid = R_centroid_index #closest_data_centroid R_centroid_indeksiksi
+            
+                
+        clusters.append(closest_data_centroid)
 
-clusters = np.array(clusters) #muutetaan saadut tulokset numpy arrayksi
-print("jotain saatiin aikaan: ", clusters)
+    final_clusters = np.array(clusters) #muutetaan saadut tulokset numpy arrayksi, laitoin nimeksi final, jotta se on eri kuin pelkkä clusters
+   
+    current_centroids = update_centroids(all_centroids, np_data, final_clusters)
 
-
+    if np.allclose(old_centroids,current_centroids, atol=1e-4):
+        print("no nyt!")
+        break
 
 #############
 ## vaihe 3 ##
@@ -108,37 +142,13 @@ print("jotain saatiin aikaan: ", clusters)
 # yhteen ja jaetaan kolmella jolloin saadaan kolme keskiarvoa joita käytetään uudella
 # kierroksella taas määrittelyyn niin pitkään etteivät pallurat enää liiku
 
-
-number_of_clusters = random_data.shape[0]
-
-
-def update_centroids(number_of_clusters, np_data, clusters):
-    new_centroids = np.zeros((number_of_clusters, np_data.shape[1]))
-   
-    for i in range(number_of_clusters):
-        cluster_points = np_data[clusters == i]
-       
-        if len(cluster_points) > 0:
-           new_centroids[i] = np.mean(cluster_points, axis=0)
-        else:
-            random_index = np.random.randint(0, len(np_data))
-            new_centroids[i] = np_data[random_index]
-            print(f"Klusteri {i} oli tyhjä -> arvottiin uusi centroidi:", new_centroids[i])
-           
-    return new_centroids
-           
-
-
-new_centroids = update_centroids(number_of_clusters, np_data, clusters)
-
-print(new_centroids)
-
-
-
+#current_centroids = update_centroids(all_centroids, np_data, clusters)
+final_centroids = current_centroids
+final_final_clusters = final_clusters
 
 
 x = np.array(data_points)
-y = np.array(random_data)
+y = final_centroids
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
